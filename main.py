80284d7d05211d857a7136b498ab7fd4244dc076
unknown <kacperugorny02@gmail.com>
2023-10-30 19:49:01 +0100

    main structure, saving data to board

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+import time
 
-url = "https://play2048.co/"
-browser = webdriver.Chrome()
-browser.get(url)
-board = [[0 for _ in range(4)] for _ in range(4)]
-board_previous = [row[:] for row in board]
-previous_res = ""
-bodyElement = browser.find_element(By.TAG_NAME, "body")
-while True:
-    soup = BeautifulSoup(browser.page_source, features="lxml")
-    res = soup.find('div', {'class': 'tile-container'})
-    if previous_res == res:
-        continue
-    for child in res.children:
-        info = child["class"]
-        posy = info[2][14]
-        posx = info[2][16]
-        tile = {"size": info[1][info[1].find('-')+1:], "posy": posy, "posx": posx}
-        print(tile)
-    bodyElement.send_keys('w')
-    previous_res = res
+
+def main():
+    url = "https://play2048.co/"
+    browser = webdriver.Chrome()
+    browser.get(url)
+    time.sleep(10)
+    board = [[0 for _ in range(4)] for _ in range(4)]
+    board_previous = [row[:] for row in board]
+    previous_res = ""
+    bodyElement = browser.find_element(By.TAG_NAME, "body")
+    while True:
+        board = [[0 for _ in range(4)] for _ in range(4)]
+        soup = BeautifulSoup(browser.page_source, features="lxml")
+        res = soup.find('div', {'class': 'tile-container'})
+        if previous_res == res:
+            continue
+        for child in res.children:
+            info = child["class"]
+            posy = int(info[2][14]) - 1
+            posx = int(info[2][16]) - 1
+            size = int(info[1][info[1].find('-')+1:])
+            if size > board[posy][posx]:
+                board[posy][posx] = size
+        print(board)
+
+        previous_res = res
+
+
+def remove_ads(browser: webdriver):
+    all_iframes = browser.find_elements(By.TAG_NAME, "iframe")
+    if len(all_iframes) > 0:
+        print("Ad Found\n")
+        browser.execute_script("""
+            var elems = document.getElementsByTagName("iframe");
+            for(var i = 0, max = elems.length; i < max; i++)
+                 {
+                     elems[i].parentNode.removeChild(elems[i]);
+                 }
+                              """)
+        print('Total Ads: ' + str(len(all_iframes)))
+    else:
+        print('No frames found')
+
+
+if __name__ == "__main__":
+    main()
